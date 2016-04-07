b609a2bf198ef2bf91a1454db675cb3c5c401126
Roman <kaboom.ua@gmail.com>
2016-04-07 22:17:19 +0300

    Update articles.js

--- src/actions/articles.js
+++ src/actions/articles.js
@@ -3,6 +3,7 @@ import {DELETE_ARTICLE} from '../constants'
 export function deleteArticle(article) {
     return {
         type: DELETE_ARTICLE,
+        //советую все данные передавать в отведенном объекте: data или payload
         deletedArticle:article
     }
 }
